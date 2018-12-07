5e0b8a367b7bc1d3e9a3aa9b79f5e53742f423c7
lanhai <1444133004@qq.com>
2018-12-07 15:28:45 +0800

    模拟hackernews完毕

--- day03/hacker-news/test.js
+++ day03/hacker-news/test.js
@@ -1,84 +1,131 @@
-let http = require('http');
 let fs = require('fs');
 let path = require('path');
 let url = require('url');
-let querystring = require('querystring');
 let mime = require('mime');
+let querystirng = require('querystring');
 let _ = require('underscore');
-
-http.createServer(function (req, res) {
+require('http').createServer(function (req, res) {
     let reqUrl = req.url.toLowerCase();
     let reqMethod = req.method.toLowerCase();
     let urlObj = url.parse(reqUrl, true);
-    let fileData = path.join(__dirname, "data", "data-test.json");
-    let list = [];
-    res.errHead = function () {
-        res.writeHead(404, 'Not Found', { "Content-Type": 'text/html;charset=utf-8' });
-        res.end('Page Not Found.');
-    }
-    res.rander = function (fileUrl, tpldata) {
-        fs.readFile(fileUrl, function (err, data) {
+    var list = [];
+    let dataPath = path.join(__dirname, "data", "data_test.json");
+    res.rander = function (filePath, xrData) {
+        fs.readFile(filePath, function (err, data) {
             if (err) {
-                res.errHead();
+                res.writeHead(404, 'Not Found', { "Content-Type": "text/html;charset=utf-8" });
                 return;
             }
-            if (tpldata) {
-                data = _.template(data.toString("utf8"))(tpldata);
+            if (xrData) {
+                data = _.template(data.toString("utf8"))(xrData);
             }
-            res.writeHead(200, 'OK', { "Content-Type": mime.getType(fileUrl) + ';charset=utf-8' });
+            res.writeHead(200, 'OK', { "Content-Type": mime.getType(filePath) + ";charset=utf-8" });
             res.end(data);
         })
     }
-    res.readData = function (fileData, list) {
-        fs.readFile(fileData, "utf8", function (err, data) {
-            if (err && err.code != 'ENOENT') throw err;
-            list.push(JSON.parse(data || '[]'));
+    if (reqUrl === '/' || reqUrl === '/index' || reqUrl === '/index.html' && reqMethod === "get") {
+        readData(function (list_n) {
+            list_n = list_n.reverse();
+            res.rander(path.join(__dirname, "views", "index.html"), { list: list_n });
         })
-    }
-    res.timeWrite = function (fileData, list) {
-        setTimeout(function () {
-            fs.writeFile(fileData, JSON.stringify(list), "utf8", function (err) {
-                if (err) throw err;
-                res.statusCode = 302;
-                res.statusMessage = 'Found';
-                res.setHeader('Location', "/");
-                res.end();
+    } else if (reqUrl === '/submit' || reqUrl === '/submit.html' && reqMethod === "get") {
+        res.rander(path.join(__dirname, "views", "submit.html"))
+    } else if (reqUrl.indexOf('/item') >= 0 && reqMethod === "get") {
+        let thisNewsId = Number(urlObj.query.id);
+        let thisNews = null;
+        readData(function (list_n) {
+            for (let i = 0; i < list_n.length; i++) {
+                if (list_n[i].id === thisNewsId) {
+                    thisNews = list_n[i];
+                    break;
+                }
+            }
+            if (thisNews) {
+                res.rander(path.join(__dirname, "views", "details.html"), { ttt: thisNews });
+            } else {
+                res.rander(path.join(__dirname, "views", "err.html"))
+            }
+        })
+    } else if (reqUrl.indexOf('/add') >= 0 && reqMethod === "get") {
+        readData(function (list) {
+            urlObj.query.id = list.length;
+            list.push(urlObj.query)
+            wirteData(JSON.stringify(list), function () {
+                redirect(res);
             })
-        }, 300);
-    }
-
-    if (reqUrl === "/" || reqUrl === "/index" || reqUrl === "/index.html" && reqMethod === "get") {
-        fs.readFile(fileData, "utf8", function (err, data) {
-            if (err && err.code != 'ENOENT') throw err;
-            let list_data = (JSON.parse(data || '[]'));
-            res.rander(path.join(__dirname, "views", "index.html"), { list: list_data });
-            
         })
-    } else if (reqUrl === "/details" || reqUrl === "/details.html" && reqMethod === "get") {
-        res.rander(path.join(__dirname, "views", "details.html"));
-    } else if (reqUrl === "/submit" || reqUrl === "/submit.html" && reqMethod === "get") {
-        res.rander(path.join(__dirname, "views", "submit.html"));
-    } else if (reqUrl.indexOf("/add") >= 0 && reqMethod === "get") {
-        res.readData(fileData, list);
-        list.push(urlObj.query);
-        res.timeWrite(fileData, list);
-    } else if (reqUrl.indexOf("/add") >= 0 && reqMethod === "post") {
-        res.readData(fileData, list);
-        let arr = [];
-        req.on("data", function (chunk) {
-            arr.push(chunk);
+    } else if (reqUrl.indexOf('/add') >= 0 && reqMethod === "post") {
+        readData(function (list) {
+            postBody(req, function (arr) {
+                arr.id = list.length;
+                list.push(arr);
+                wirteData(JSON.stringify(list), function () {
+                    redirect(res);
+                })
+            })
         })
-        req.on("end", function () {
-            arr = Buffer.concat(arr);
-            arr = arr.toString('utf-8');
-            list.push(querystring.parse(arr));
-        })
-        res.timeWrite(fileData, list);
-    } else if (reqUrl.indexOf("/resources") >= 0 && reqMethod === "get") {
-        res.rander(path.join(__dirname, reqUrl));
+    } else if (reqUrl.indexOf('/resources') >= 0 && reqMethod === "get") {
+        res.rander(path.join(__dirname, reqUrl))
     } else {
-        res.errHead();
+        res.writeHead(404, 'Not Found', { "Content-Type": "text/html;charset=utf-8" });
+        res.end("<h1>Page Not Found.</h1>")
     }
-}).listen(8081, function () {
-    console.log('服务器启动成功，请访问http://localhost:8081');
+}).listen(8080, function () {
+    console.log('服务器启动成功请访问http://localhost:8080')
 })
+
+/**
+ * 读取数据
+ * @param {*} callback 回调函数
+ */
+function readData(callback) {
+    let dataPath = path.join(__dirname, "data", "data_test.json");
+    fs.readFile(dataPath, "utf8", function (err, data) {
+        if (err && err.code != 'ENOENT') throw err;
+        let list_n = JSON.parse(data || "[]");
+        callback(list_n);
+    })
+}
+/**
+ * 写入数据
+ * @param {string} list 数据
+ * @param {*} callback 回调函数
+ */
+function wirteData(list, callback) {
+    let dataPath = path.join(__dirname, "data", "data_test.json");
+    fs.writeFile(dataPath, list, "utf8", function (err) {
+        if (err) throw err;
+        // 回调函数 读取数据后处理代码
+        callback();
+    })
+}
+/**
+ * post请求的数据
+ * @param {*} req 
+ * @param {*} callback 回调函数
+ */
+function postBody(req, callback) {
+    let arr = [];
+    // 监听data事件 用户传来的数据
+    req.on('data', function (chunk) {
+        arr.push(chunk);
+    })
+    // 监听end事件，数据接受完毕
+    req.on('end', function () {
+        arr = Buffer.concat(arr);
+        arr = arr.toString("utf8");
+        arr = querystirng.parse(arr);
+        // 回调函数 返回数据
+        callback(arr);
+    })
+}
+/**
+ * 重定向 跳转页面
+ * @param {*} res 
+ */
+function redirect(res) {
+    res.statusCode = 302;
+    res.statusMessage = 'Not Found';
+    res.setHeader('Location', '/');
+    res.end();
+}
